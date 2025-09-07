abf9e769d734c66e24dc5e0fcde21731895bfe6c
CharanBharathula <bcharan197@gmail.com>
2025-09-07 22:10:36 +0530

    added basic APIs for add and view

---
+++ todobackend/backend/src/main/java/com/example/backend/controller/TodoController.java
@@ -0,0 +1,46 @@
+package com.example.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.backend.model.Todo;
+import com.example.backend.repository.TodoRepo;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    
+    @Autowired
+    private TodoRepo todoRepo;
+    
+    // Get all todos
+    @GetMapping
+    public ResponseEntity<List<Todo>> getAllTodos() {
+        List<Todo> todos = todoRepo.getAllTodos();
+        return ResponseEntity.ok(todos);
+    }
+    
+    // Add a new todo
+    @PostMapping
+    public ResponseEntity<String> addTodo(@RequestBody Todo todo) {
+        Todo savedTodo = todoRepo.saveTodo(todo);
+        if( savedTodo == null ) {
+            return ResponseEntity.status(500).body("Error saving todo");
+        }
+        return ResponseEntity.ok("Todo added successfully");
+    }
+    
+    // Get a todo by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable int id) {
+        Todo todo = todoRepo.getTodoById(id);
+        if (todo != null) {
+            return ResponseEntity.ok(todo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ todobackend/backend/src/main/java/com/example/backend/model/Todo.java
@@ -0,0 +1,34 @@
+package com.example.backend.model;
+
+
+public class Todo {
+    private String id;
+    private String title;
+    private boolean completed;
+
+    // Constructors, getters, and setters
+    public Todo() {}
+    public Todo(String id, String title, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
