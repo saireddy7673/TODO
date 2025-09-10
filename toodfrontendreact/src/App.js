434f166a30bfb8d58749094a1dccccf3290e2e7b
CharanBharathula <bcharan197@gmail.com>
2025-09-10 13:24:12 +0530

    Remove todobackend folder and update structure

--- toodfrontendreact/src/App.js
+++ toodfrontendreact/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get('https://localhost:8080/todos');
+      setTodos(response.data);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map( todo => {
+          <li key={todo.id}>{todo.title}</li>
+        })}
+      </ul>
+      
     </div>
   );
 }
